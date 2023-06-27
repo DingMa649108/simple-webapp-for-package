ba2832593cd972ef02d0fe72cbd071e7153e6e88
DINMA <dinma@bcldb.com>
2023-06-27 15:17:13 -0700

    FC

---
+++ src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
@@ -0,0 +1,30 @@
+package com.example.consumingwebservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.consumingwebservice.wsdl.Add;
+import com.example.consumingwebservice.wsdl.AddResponse;
+
+@SpringBootApplication
+public class ConsumingWebServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumingWebServiceApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner lookup(EVClient quoteClient) {
+    return args -> {
+
+		AddResponse response = quoteClient.getResult(3, 7);
+    System.err.println("the add result is: " + response.getAddResult());
+    };
+  }
+
+}
---
+++ src/main/java/com/example/consumingwebservice/EVClient.java
@@ -0,0 +1,53 @@
+package com.example.consumingwebservice;
+
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
+import org.springframework.ws.transport.http.HttpUrlConnection;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.namespace.QName;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpRequest;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.HttpException;
+
+import com.example.consumingwebservice.wsdl.Add;
+import com.example.consumingwebservice.wsdl.AddResponse;
+
+public class EVClient extends WebServiceGatewaySupport {
+
+    public AddResponse getResult(int a, int b) {
+        final QName soapHeaderName = new QName("SOAPAction", "");
+		Add request = new Add();
+		request.setIntA(a);
+		request.setIntB(b);
+
+	    final WebServiceMessageCallback actionCallback = new SoapActionCallback("http://tempuri.org/Add");
+	    final WebServiceMessageCallback customCallback = new WebServiceMessageCallback() {
+	        public void doWithMessage(WebServiceMessage message) throws IOException, TransformerException {
+	            actionCallback.doWithMessage(message);
+	            // SoapMessage soap = (SoapMessage) message;
+
+            
+				
+	        }
+	    };	
+        return (AddResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", request, customCallback);
+    }
+}
