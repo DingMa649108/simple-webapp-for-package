330dd0192b49e042cf7a8a1dcafe0e3cc7946833
DINMA <dinma@bcldb.com>
2023-07-12 10:04:56 -0700

    FC

---
+++ src/test/java/com/example/consumingwebservice/TestClient.java
@@ -0,0 +1,8 @@
+package com.example.consumingwebservice;
+
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = EVConfig.class)
+public class TestClient {
+    
+}
